d7da24de5707fda86448af2ae3a17a462a396723
wPlachno <wjplachno@gmail.com>
2024-12-25 11:57:14 -0800

    12/25/24 - Convert main wctk usage to use WCCore system. (wplachno)

--- wctk.py
+++ wctk.py
@@ -4,7 +4,10 @@ import sys
 from interface.constants import MODE
 from interface.wctk_cli import WoodchipperToolkitCommandLineInterface as WCCLI
 from interface.wctk_controller import WoodchipperController as WCController
-
+from utilities.wccore import WoodchipperCore as WCCore
+import interface.wctk_request as my_parser
+import core.wtck_handlers as handlers
+import interface.wctk_printer as printers
 
 
 """ Todo
@@ -15,12 +18,15 @@ from interface.wctk_controller import WoodchipperController as WCController
 
 
 def _main(args):
-    cli = WCCLI()
-    control = WCController()
-    process_request = cli.process_request(sys.argv)
-    if not process_request.mode == MODE.NONE:
-        control.process_request(process_request)
-        cli.display_results(control.results)
+    core = WCCore()
+    core.set_parser_builder(my_parser.build_parser)
+    core.set_debug_mode_description("No file editing will be completed when debug mode is active.")
+    core.add_mode(MODE.SHOW, handlers.WoodchipperHandlerShow, printers.WoodchipperToolkitPrinterShow)
+    core.add_mode(MODE.ADD, handlers.WoodchipperHandlerAdd, printers.WoodchipperToolkitPrinterAdd)
+    core.add_mode(MODE.PUSH, handlers.WoodchipperHandlerPush, printers.WoodchipperToolkitPrinterPush)
+    core.add_mode(MODE.GRAB, handlers.WoodchipperHandlerGrab, printers.WoodchipperToolkitPrinterGrab)
+    core.run()
+
 
 if __name__ == "__main__":
     _main(sys.argv)
