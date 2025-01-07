0cbc861ca9b7dae5ff44b14f01f8c30c000d372f
wPlachno <wjplachno@gmail.com>
2025-01-07 08:19:12 -0800

    01/07/25 - Added on_bool() and made all new_line aware. (wplachno)

--- utilities/wcprinter.py
+++ utilities/wcprinter.py
@@ -1,6 +1,6 @@
 # wcprinter.py
-# Version: 0.0.1.010
-# Last Changes: 01/01/2025
+# Version: 0.0.1.011
+# Last Changes: 01/07/2025
 
 from utilities.wcconstants import Verbosity, PRINT
 
@@ -19,17 +19,21 @@ class WoodchipperToolkitPrinter:
     def nl(self, verbosity=Verbosity.NORMAL):
         self.pr(text="", verbosity=verbosity)
 
-    def error(self, text, verbosity=Verbosity.NORMAL):
-        self.pr(PRINT.ERROR.format(text), verbosity)
+    def error(self, text, verbosity=Verbosity.NORMAL, new_line=True):
+        self.pr(PRINT.ERROR.format(text), verbosity=verbosity, new_line=new_line)
 
-    def label(self, text, verbosity=Verbosity.NORMAL):
-        self.pr(PRINT.LABEL.format(text), verbosity)
+    def label(self, text, verbosity=Verbosity.NORMAL, new_line=True):
+        self.pr(PRINT.LABEL.format(text), verbosity=verbosity, new_line=new_line)
 
-    def kvp(self, key, value, verbosity=Verbosity.NORMAL):
-        self.pr(PRINT.KVP.format(key, str(value)), verbosity)
+    def kvp(self, key, value, verbosity=Verbosity.NORMAL, new_line=True):
+        self.pr(PRINT.KVP.format(key, str(value)), verbosity=verbosity, new_line=new_line)
 
-    def v_frame(self, frame_list, *args):
-        self.pr(frame_list[self.verbosity].format(*args), Verbosity.RESULTS_ONLY)
+    def on_bool(self, condition, if_true, if_false, verbosity=Verbosity.NORMAL, new_line=True):
+        print_st = if_true if condition else if_false
+        self.pr(print_st, verbosity=verbosity, new_line=new_line)
+
+    def v_frame(self, frame_list, *args, new_line=True):
+        self.pr(frame_list[self.verbosity].format(*args), new_line=new_line)
 
 
 
